33632dd73cbf4cb65db25559cb14650e87e9f873
Rahel <V>
2025-01-07 17:38:54 +0100

    update default angles

--- src/Constants.h
+++ src/Constants.h
@@ -7,17 +7,20 @@
 
 // Constants related to the body dimensions of the robot
 namespace Constants {
-    constexpr double UPPERLEG = 10.0;
-    constexpr double LOWERLEG = 10.0;
+    constexpr double LIMB = 10.0;
+    // constexpr double LOWERLEG = 10.0;
     constexpr double SHOULDER = 6.1;
 
     constexpr double BASEHEIGHT = 6.0;
     constexpr double BASESIDELEGEXTEND = 0.5;
     constexpr double BASEFRONTELEGEXTEND = 6.1;
 
-    constexpr int BODYDEFAULTANGLESERVO = 90;
-    constexpr int SHOULDERDEFAULTANGLESERVO = 90;
+    constexpr int BODYDEFAULTANGLESERVO = 85;
+    constexpr int SHOULDERDEFAULTANGLESERVO = 0; //Right side
+    constexpr int LEFT_SHOULDERDEFAULTANGLESERVO = 180;
     constexpr int KNEEDEFAULTANGLESERVO = 0;
+    constexpr int LEFT_KNEEDEFAULTANGLESERVO = 180;
+
     constexpr int BODYDEFAULTANGLE = 90;
     constexpr int SHOULDERDEFAULTANGLE = 64;
     constexpr int KNEEDEFAULTANGLE = 35;
