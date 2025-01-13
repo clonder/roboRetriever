e561584e4cccfcdf5efa0563dafe63047f683205
Clonder <st.merkush@gmail.com>
2025-01-13 16:05:53 +0100

    Added wifi

--- src/main.cpp
+++ src/main.cpp
@@ -2,70 +2,110 @@
 #include <InverseKinematics.h>
 #include <string>
 #include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <vector>
 
 using namespace std;
+
+const char* ssid = "Clonder";
+const char* password = "1234567812";
+
+AsyncWebServer server(80);
 InverseKinematics ik;
+bool wifiConnected = false;
 
 void setup()
 {
     // Setup serial
     Serial.begin(115200);
-    Serial.println("Starting setup sequence\n");
+    while (!Serial) {
+        ; // Wait for serial port to connect. Needed for native USB port only
+    }
+
+    // Connect to Wi-Fi
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+    }
 
     // Position servos into default position
     ik.Start();
     delay(1000);
 
-    Serial.println("Finished setup sequence\n");
+    server.on("/test", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(200, "text/plain", "Test endpoint reached");
+    });
+
+    server.on("/command", HTTP_GET, [](AsyncWebServerRequest *request){
+        if (request->hasParam("cmd")) {
+            String command = request->getParam("cmd")->value();
+            string data = command.c_str();
+            // Split the commands by semicolon
+            vector<string> commands;
+            size_t pos = 0;
+            while ((pos = data.find(";")) != string::npos) {
+                commands.push_back(data.substr(0, pos));
+                data.erase(0, pos + 1);
+            }
+            commands.push_back(data);
+
+            // Execute each command
+            for (const string& cmd : commands) {
+                if (cmd.find("y") != -1) {
+                    int y = stoi(cmd.substr(1, cmd.length()));
+                    ik.moveY(y);
+                } else if (cmd.find("w") != -1) {
+                    int steps = stoi(cmd.substr(1, cmd.length()));
+                    ik.moveForward(steps);
+                } else if (cmd.find("z") != -1) {
+                    int z = stoi(cmd.substr(1, cmd.length()));
+                    if (z >= 6 && z <= 19) {
+                        ik.moveZ(z);
+                    }
+                } else if (cmd.find("ft") != -1) {
+                    int z = stoi(cmd.substr(2, cmd.length()));
+                    if (z >= 6 && z <= 19) {
+                        ik.tilt(z, FORWARD);
+                    }
+                } else if (cmd.find("bt") != -1) {
+                    int z = stoi(cmd.substr(2, cmd.length()));
+                    if (z >= 6 && z <= 19) {
+                        ik.tilt(z, BACKWARD);
+                    }
+                } else if (cmd.find("RESET") != -1) {
+                    ik.Start();
+                } else {
+                    request->send(400, "text/plain", "Invalid command: " + String(cmd.c_str()));
+                    return;
+                }
+                delay(500);
+            }
+            request->send(200, "text/plain", "Command sequence executed");
+        } else {
+            request->send(400, "text/plain", "No command sent");
+        }
+    });
+
+    server.begin();
 }
 
-// Main thread where programm runs
 void loop()
 {
-    if (Serial.available())
-    {
-        String data_1 = Serial.readStringUntil('\n');
-        Serial.println(data_1);
-        string data = data_1.c_str();
-        // move leg forward
-        if (data.find("y") != -1)
-        {
-            int y = stoi(data.substr(1, data.length()));
-            ik.moveY(y);
+    // Attempt to connect to WiFi until connected
+    if (!wifiConnected) {
+        WiFi.begin(ssid, password);
+        unsigned long startAttemptTime = millis();
+
+        // Wait for connection
+        while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 10000) {
+            delay(500);
         }
-        // walk forward
-        else if (data.find("w") != -1)
-        {
-            int steps = stoi(data.substr(1, data.length()));
-            ik.moveForward(steps);
-        }
-        // move up and down
-        else if (data.find("z") != -1)
-        {
-            int z = stoi(data.substr(1, data.length()));
-            if (z >= 6 | z <= 19) {
-                ik.moveZ(z);
-            }
-        }
-        // tilt forward
-        else if (data.find("ft") != -1)
-        {
-            int z = stoi(data.substr(2, data.length()));
-            if (z >= 6 | z <= 19) {
-                ik.tilt(z, FORWARD);
-            }
-        }
-        // tilt backward
-        else if (data.find("bt") != -1)
-        {
-            int z = stoi(data.substr(2, data.length()));
-            if (z >= 6 | z <= 19) {
-                ik.tilt(z, BACKWARD);
-            }
-        }
-        else if (data.find("RESET") != -1)
-        {
-            ik.Start();
+
+        if (WiFi.status() == WL_CONNECTED) {
+            wifiConnected = true;
         }
     }
+
+    // Add a small delay to avoid flooding the Serial Monitor
+    delay(1000);
 }
